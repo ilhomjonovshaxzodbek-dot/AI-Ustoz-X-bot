6ed23cc06281c66cf05e34e6e6264c4849219a75
ilhomjonovshaxzodbek-dot <ilhomjonovshaxzodbek@gmail.com>
2026-06-14 22:43:07 +0500

    Add configuration for bot tokens and channels

---
+++ config.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+CHANNELS = [
+    "@shzodbekcoderdev",
+    "@faylmasteruzbot", 
+    "@hisobchixuzbot"
+]
+
+LANGUAGES = {
+    "uz": "🇺🇿 O'zbek",
+    "ru": "🇷🇺 Rus",
+    "en": "🇬🇧 Ingliz"
+}
