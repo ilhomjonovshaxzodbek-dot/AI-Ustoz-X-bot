05619954e471e8c04815e0ec8bf89563ca2e8f5a
ilhomjonovshaxzodbek-dot <ilhomjonovshaxzodbek@gmail.com>
2026-06-15 08:20:40 +0500

    Update gemini.py

--- utils/gemini.py
+++ utils/gemini.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 from config import GEMINI_API_KEY
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 async def gemini_request(prompt: str) -> str:
     try:
